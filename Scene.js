125fc4ac82cec2f0b3396f4f2645b01e4a072e7f
Park Soyoung <thdud00627@gmail.com>
2024-06-09 23:41:16 +0900

    p5.js 오류 수정

--- Scene.js
+++ Scene.js
@@ -1,11 +1,5 @@
 class Scene {
   enterTime;
-  constructor() {
-    if (new.target == Scene) {
-      throw new TypeError("Cannot construct Scene instances directly");
-    }
-    this.enterTime = 0;
-  }
   OnEnter() { }
   OnDraw() { }
   OnExit() { }
